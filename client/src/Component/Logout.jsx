b29e3ea64dde334cc96e383948abe3d66fd92767
rohit.singh <rohitsingh94874@gmail.com>
2025-05-20 08:17:37 +0530

    get the info of participants

--- client/src/Component/Logout.jsx
+++ client/src/Component/Logout.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../authContext/AuthContext";
+import toast from 'react-hot-toast'
 
 const Logout = () => {
     const { setAuthUser, setUser } = useAuthContext();
@@ -12,6 +13,7 @@ const Logout = () => {
         setUser(null)
 
         console.log("Item removed");
+        toast.success("Logout Succesfully");
         navigate("/");
     }
     return (
